1dbe0fa3d869fc0978f413a99cffd790d13b3647
Rave JS <hswatter@mail.ru>
2024-08-02 15:20:38 +0300

    page routing

---
+++ src/app/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Home from '@/pages/index.vue'
+</script>
+
+<template>
+  <Home msg="hey" />
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
