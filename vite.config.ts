1f8ba55ef5a3253f181052a260010df5723b709e
Rave JS <hswatter@mail.ru>
2024-08-02 10:34:13 +0300

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    open: true,
+    port: 5173,
+    hmr: {
+      host: 'localhost',
+      port: 5173
+    }
+  }
 })
