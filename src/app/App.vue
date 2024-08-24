86d5c6f88ebbce830dd2228cfc118123555d521f
Rave JS <hswatter@mail.ru>
2024-08-24 13:59:22 +0300

    changed App component

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,13 +1,9 @@
+<template>
+    <Home />
+</template>
+
 <script setup lang="ts">
 import Home from '@/pages/index.vue'
 </script>
 
-<template>
-  <Home msg="hey" />
-</template>
-
-<style scoped>
-h1 {
-  text-align: center;
-}
-</style>
+<style scoped></style>
