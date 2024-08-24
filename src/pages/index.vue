28800952ae4fb9b6786e606cfa6ac5759c3bb85b
Rave JS <hswatter@mail.ru>
2024-08-24 14:00:41 +0300

    made layout with slot and mocked data

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,40 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>pages/index.vue</code> to test HMR
-    </p>
-  </div>
+  <Layout>
+    <div class='burger-container'>
+      <ul v-for="burger in burgers">
+        <li><img src="/src/shared/assets/images/burger.jpg" alt="burger" /></li>
+      </ul>
+    </div>
+  </Layout>
 </template>
 
+<script setup lang="ts">
+import { reactive } from 'vue'
+import Layout from '@/widgets/layout'
+
+const burgers = reactive([1, 2, 3, 4, 5])
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
+img {
+  width: 200px;
+  border-radius: 5px;
+}
+ul > li {
+  list-style: none;
+}
+button {
+  background-color: aqua;
+  border-radius: 7px;
+  border: none;
+  cursor: pointer;
+  filter: drop-shadow(5px 5px 5px rgb(88, 88, 188));
+}
+.burger-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+	flex-wrap: wrap;
+	flex-basis: 50%;
 }
 </style>
---
+++ src/widgets/layout/Layout/Layout.vue
@@ -0,0 +1,60 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <router-link to="/">
+          <li>
+            <img :src="burger" />
+          </li>
+        </router-link>
+      </ul>
+    </nav>
+  </header>
+  <div class="container">
+    <main>
+			<h1>Menu</h1>
+      <slot></slot>
+    </main>
+  </div>
+  <footer></footer>
+</template>
+
+<script setup lang="ts">
+import burger from '@/shared/assets/icons/icon.png'
+</script>
+
+<style scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  background-color: gray;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100dvh;
+  width: 100%;
+  background-color: #f5e2ba;
+  margin-top: 70px;
+}
+.container > main {
+  width: 60%;
+  height: 90%;
+  background-color: #ffffff;
+  border-radius: 7px;
+  text-align: center;
+}
+img {
+  width: 55px;
+}
+li {
+  list-style: none;
+}
+h1 {
+	text-align: center;
+}
+</style>
